7234d4bceee83eb318c0b5107544a9fa79d26e63
nosianuri <nurinosia@gmail.com>
2022-07-21 02:33:17 +0600

    update channel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import { ChannelListContainer, ChannelContainer} from './components';
 
-const apikey =  '3agdevg8pmym';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
-const client = StreamChat.getInstance(apikey);
+import 'stream-chat-react/dist/css/index.css';
+import './App.css';
 
-function App() {
+const cookies = new Cookies();
+
+const apiKey = '3agdevg8pmym';
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+  client.connectUser({
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
+  }, authToken)
+}
+
+
+const App = () => {
+
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (!authToken) return <Auth />
+
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme="team light">
         <ChannelListContainer
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
         />
 
         <ChannelContainer
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
         />
       </Chat>
     </div>
