0d50f2826c137a0e4eaba22ab06c699f1aa6683e
nosianuri <nurinosia@gmail.com>
2022-07-20 09:09:32 +0600

    Sidebar set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelListContainer, ChannelContainer} from './components';
+
+const apikey =  '3agdevg8pmym';
+
+const client = StreamChat.getInstance(apikey);
 
 function App() {
   return (
-    <div>
-      <h1>Medical pager Chat Application</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme="team light">
+        <ChannelListContainer
+
+        />
+
+        <ChannelContainer
+
+        />
+      </Chat>
     </div>
   );
 }
